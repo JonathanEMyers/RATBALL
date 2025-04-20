aea5f25b8c7b98a80a6e139a05d538e9643967aa
JonathanEMyers <152437431+JonathanEMyers@users.noreply.github.com>
2025-04-19 23:53:43 -0500

    Initial commit of combined files to new main branch

--- src/Microphone.py
+++ src/Microphone.py
@@ -1,6 +1,21 @@
 import collections
 import alsaaudio as aa
 from datetime import datetime, timezone
+import struct
+
+# This is a function to determine the time since epoch
+unix_epoch = datetime.fromtimestamp(0, timezone.utc)
+def unix_time_millis(dt):
+    """formats timestamps as milliseconds-since-epoch (double-precision float, only requires 8 bytes)"""
+    if dt.tzinfo is None:
+        # make dt offset-aware in UTC if it's naive
+        dt = dt.replace(tzinfo=timezone.utc)
+    else:
+        # convert to UTC if it's aware in a different timezone
+        dt = dt.astimezone(timezone.utc)
+    return (dt - unix_epoch).total_seconds() * 1000.0
+
+
 
 class Microphone:
     def __init__(self, bufSize, rate, channels, format, framerate):
@@ -20,28 +35,40 @@ class Microphone:
                                periodsize=chunkSize)
         
         self.bufferSize = bufSize
+
+        # Just for debugging purposes
+        self.frameCount = 0
         
 
         
     def appendMicData(self, whichBuffer):
         length, data = self.micInput.read()
+        timeNow = datetime.now()
+        timeSinceEpoch = unix_time_millis(timeNow)
+        timeSinceEpochPacked = struct.pack('d', timeSinceEpoch)
 
         if(whichBuffer and len(self.dataBufferOne) < self.bufferSize):
             self.dataBufferOne.append(data)
-            self.metaBufferOne.append(b'00000000')
+            self.metaBufferOne.append(timeSinceEpochPacked)
+            self.frameCount += 1
+            return(True)
 
         elif(not whichBuffer and len(self.dataBufferTwo) < self.bufferSize):
             self.dataBufferTwo.append(data)
-            self.metaBufferTwo.append(b'00000000')
+            self.metaBufferTwo.append(timeSinceEpochPacked)
+            self.frameCount += 1
+            return(True)
 
         else:
-            return("In Microphone.appendMicData() -- Both buffers are full!")
+            return(False)
         
 
 
-    
 
     def popMicData(self, whichBuffer):
+
+        # print(f"whichBuffer = {whichBuffer}")
+        # print(f"DB1={len(self.dataBufferOne)}, MB1={len(self.metaBufferOne)}, DB2={len(self.dataBufferTwo)}, MB2={len(self.metaBufferTwo)}")
         
         if(not whichBuffer and len(self.dataBufferOne) > 0):
             data = self.dataBufferOne.popleft()
