5de0bb1373c168ff5d1009a735e55964efe76c3a
Adria Wesson <adria.wesson@gmail.com>
2025-04-25 13:32:22 -0500

    implement local-only hardware test script draft

--- scripts/hardware_test.py
+++ scripts/hardware_test.py
@@ -1,8 +1,13 @@
+import qwiic_otos
 import threading
 import time
 import sys
 import os
-import datetime
+from datetime import datetime, timezone
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+matplotlib.use('qtagg')
 
 self_path = os.path.abspath(__file__)
 self_dir = os.path.split(self_path)[0]
@@ -14,35 +19,35 @@ for sensor in sensor_manifest:
     sensor.begin()
 
 def unix_time_millis(dt):
-    """formats timestamps as milliseconds-since-epoch (double-precision float, only requires 8 bytes)"""
     unix_epoch = datetime.fromtimestamp(0, timezone.utc)
-
     if dt.tzinfo is None:
-        # make dt offset-aware in UTC if it's naive
         dt = dt.replace(tzinfo=timezone.utc)
     else:
-        # convert to UTC if it's aware in a different timezone
         dt = dt.astimezone(timezone.utc)
     return (dt - unix_epoch).total_seconds() * 1000.0
 
 
 class SensorPlotter():
-    def __init__(self, dataClass):
-        self._dataClass = dataClass
-        self.hLine = plt.plot(0,0)
-        self.ani = FuncAnimation(plt.gcf(), self.run, interval = 1000, repeat=True)
+    def __init__(self, sensor_data):
+        self._sensor_data = sensor_data
+        self.lines = [plt.plot(0,0)[0] for _ in range(4)]
+        self.ani = FuncAnimation(plt.gcf(), self.run, interval = 10, repeat=True)
 
     def run(self, i):
-        self.hLine.set_data(self._dataClass.x, self._dataClass.y)
-        self.hLine.axes.relim()
-        self.hLine.axes.autoscale_view()
+        vals = [self._sensor_data.x, self._sensor_data.vx, self._sensor_data.y, self._sensor_data.vy]
+        for j, line in enumerate(self.lines):
+            line.set_data(self._sensor_data.time, vals[j])
+            line.axes.relim()
+            line.axes.autoscale_view()
 
 
 class SensorData():
     def __init__(self):
         self.time = []
         self.x = []
+        self.vx = []
         self.y = []
+        self.vy = []
         self.h = []
 
 
@@ -61,11 +66,14 @@ class SensorPoll(threading.Thread):
         while True:
             data = self.device.getPosVelAcc()
             self._sensor_data.time.append(unix_time_millis(datetime.now()))
-            self._sensor_data.x.append(data.x)
-            self._sensor_data.y.append(data.y)
-            self._sensor_data.h.append(data.h)
+            self._sensor_data.x.append(data[0].x)
+            self._sensor_data.vx.append(data[1].x)
+            self._sensor_data.y.append(data[0].y)
+            self._sensor_data.vy.append(data[1].y)
+            self._sensor_data.h.append(data[0].h)
             self._nextCall = self._nextCall + self._period
             time.sleep(self._nextCall - time.time())
+
 
 
 data = SensorData()
