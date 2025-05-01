f087184b2542c319fb721fa2ac5c41f81b257978
Adria Wesson <adria.wesson@gmail.com>
2025-05-01 01:04:32 -0500

    fix import for nth time

--- src/sensor.py
+++ src/sensor.py
@@ -7,7 +7,7 @@ import os.path
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from utils import unix_time_millis
+from .utils import unix_time_millis
 
 
 class Sensor:
