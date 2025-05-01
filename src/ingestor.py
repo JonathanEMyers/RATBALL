2a78496691351fa24a23bf7f1cfd264a8db1668c
Adria Wesson <adria.wesson@gmail.com>
2025-05-01 14:36:01 -0500

    fix priority queue accessor

--- src/ingestor.py
+++ src/ingestor.py
@@ -196,10 +196,14 @@ class IngestorService:
     def consume_sensor_feed(self):
         while True:
             if not self.connection_pool.empty():
-                dev_conn_wrapped = self.connection_pool.get()
+                # pop prioritized connection from PriorityQueue, unpack tuple
+                prio, device_connection = self.connection_pool.get()
+
+                # unpack device descriptor fields from DeviceGovernorConnection
                 device_type, ident, created_ts, sock = itemgetter('device_type', 'ident', 'created_ts', 'sock')(
-                    dev_conn_wrapped['items']
+                    device_connection
                 )
+                # reprioritize with a new timestamp
                 dt = time.time()*1000 - created_ts
                 # if the device isn't what we're looking for, reprioritize and re-enqueue it
                 if device_type != 'sensor':
