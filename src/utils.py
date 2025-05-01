0083df541e3b89b76ce0697ffa8d26162d1a5808
Adria Wesson <adria.wesson@gmail.com>
2025-05-01 13:46:02 -0500

    more timestamp/errmsg fixes

--- src/utils.py
+++ src/utils.py
@@ -17,10 +17,17 @@ def unix_time_millis(dt: float) -> float:
 
 def build_client_hello(device_name: str, device_ident: int) -> bytes:
     try:
-        return struct.pack(">6sId", device_name.encode("utf-8"), device_ident, unix_time_millis(dt.now()))
+        device_name_enc = device_name.encode(encoding="ascii")
+        if len(device_name_enc) != 6:
+            logger.critical(f"Got invalid size for encoded device name: {len(device_name_enc)}")
+            raise Exception(f"Failed to build client hello for device {device_name} with ident {ident}")
+        return struct.pack(">6sId", device_name_enc, device_ident, unix_time_millis(dt.now()))
     except struct.error as ex:
         exmsg = safe_unwrap_exception(ex)
-        logger.error(f"Exception occurred while packing client hello packet for device {device_name}{device_ident}: {exmsg}")
+        logger.error(f"Struct error occurred while building client hello packet for device {device_name}{device_ident}: {exmsg}")
+    except Exception as ex:
+        exmsg = safe_unwrap_exception(ex)
+        logger.error(f"Exception occurred while building client hello for device {device_name}{device_ident}: {exmsg}")
 
 
 def safe_unwrap_exception(ex: Optional[Exception]) -> str:
